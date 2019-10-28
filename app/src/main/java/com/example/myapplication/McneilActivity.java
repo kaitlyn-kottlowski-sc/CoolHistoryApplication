2057bf29860514e28f02994d36707f1c9237b451
kaitlyn-kottlowski-sc <kaitlyn.kottlowski@my.simspon.edu>
2019-10-28 16:49:40 -0500

    Back Buttons

--- app/src/main/java/com/example/myapplication/McneilActivity.java
+++ app/src/main/java/com/example/myapplication/McneilActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 
 import android.text.method.LinkMovementMethod;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -26,10 +28,31 @@ public class McneilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mcneil);
 
+//        Intent intent = getIntent();
+//        String layout = intent.getExtras().getString("Layout");
+//
+//        int id = getResources().getIdentifier(layout, "layout", getPackageName());
+//
+//        int id = this.getResources().getString(layout);
+//        setContentView(id);
 
-        Objects.requireNonNull(getSupportActionBar()).setTitle("McNeil");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView text = findViewById(R.id.link_Computer_Science);
         text.setMovementMethod(LinkMovementMethod.getInstance());
-    }}
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // API 5+ solution
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
