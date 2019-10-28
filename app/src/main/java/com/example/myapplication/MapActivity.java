00bc8ebff11ffdb787a3770534fbef96afcd2f7f
kaitlyn-kottlowski-sc <kaitlyn.kottlowski@my.simspon.edu>
2019-10-28 14:40:44 -0500

    Fixes!!

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -1,14 +1,18 @@
 package com.example.myapplication;
 
-
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.app.ActionBar;
+import android.Manifest;
 import android.content.Intent;
-import android.os.Build;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
-
 import android.os.Looper;
 import android.view.View;
 
@@ -20,38 +24,75 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.SettingsClient;
-
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private GoogleMap map;
+    SupportMapFragment mapFrag;
+    LocationRequest mLocationRequest;
+    Marker mCurrLocationMarker;
+    FusedLocationProviderClient mFusedLocationClient;
+    protected static final int REQUEST_FINE_LOCATION = 0x1;
+
+    //@RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        startLocationUpdates();
 
 
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.simpson_map);
-        assert mapFragment != null;
-        mapFragment.getMapAsync(this);
-
+        mFusedLocationClient = getFusedLocationProviderClient(this);
+        mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.simpson_map);
+        assert mapFrag != null;
+        mapFrag.getMapAsync(this);
 
 
     }
 
+    protected void startLocationUpdates() {
+
+        final long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+        final long FASTEST_INTERVAL = 2000;
+
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
 
 
     @Override
@@ -62,23 +103,33 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         // Set map to satellite mode
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
+
+        if(checkPermissions()) {
+            googleMap.setMyLocationEnabled(true);
+        }
+
         // Zoom in vicinity of Simpson Campus
         final LatLng simpson = new LatLng(41.365475, -93.564849);
         final CameraUpdate newLoc = CameraUpdateFactory.newLatLngZoom(simpson, 17);
         map.animateCamera(newLoc);
+
 
         // Initialize Building LatLng and Markers
         final LatLng wallaceHallLatLng = new LatLng(41.365112, -93.562948);
         final LatLng maryBerryLatLng = new LatLng(41.365537, -93.564544);
         final LatLng carverHallLatLng = new LatLng(41.363742, -93.563243);
         final LatLng collegeHallLatLng = new LatLng(41.364976, -93.563648);
-        final LatLng amyRobinsonLatLng = new LatLng(41.364743, -93.562664);
         final LatLng smithChapelLatLng = new LatLng(41.364191, -93.562678);
         final LatLng hopperGymLatLng = new LatLng(41.365530, -93.565462);
         final LatLng cowlesLatLng = new LatLng(41.365907, -93.565964);
         final LatLng mcNeillHallLatLng = new LatLng(41.364399, -93.564544);
         final LatLng hillmanHallLatLng = new LatLng(41.364231, -93.564044);
-        // TO DO - (LatLng) Barker, Great/Pfieffer, Kresge, Kent
+        final LatLng bpacLatLng = new LatLng(41.365412, -93.567308);
+        final LatLng kentLatLng = new LatLng(41.366596, -93.565547);
+        final LatLng lekburgHallLatLng = new LatLng(41.364779, -93.562696);
+        final LatLng greatPfiefferLatLng = new LatLng(41.365849, -93.563772);
+        final LatLng buxtonStadiumLatLng = new LatLng(41.364268, -93.565690);
+        final LatLng dunnLibraryLatLng = new LatLng(41.365502, -93.563619);
 
         final Marker wallaceHall = map.addMarker(new MarkerOptions().position(wallaceHallLatLng)
                 .title("Wallace Hall"));
@@ -88,8 +139,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .title("Carver Hall"));
         final Marker collegeHall = map.addMarker(new MarkerOptions().position(collegeHallLatLng)
                 .title("College Hall"));
-        final Marker amyRobinson = map.addMarker(new MarkerOptions().position(amyRobinsonLatLng)
-                .title("Amy Robinson Music Center"));
         final Marker smithChapel = map.addMarker(new MarkerOptions().position(smithChapelLatLng)
                 .title("Smith Chapel"));
         final Marker hopperGym = map.addMarker(new MarkerOptions().position(hopperGymLatLng)
@@ -100,223 +149,115 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .title("McNeill Hall"));
         final Marker hillmanHall = map.addMarker(new MarkerOptions().position(hillmanHallLatLng)
                 .title("Hillman Hall"));
-        // TO DO - (Marker) Barker, Great/Pfieffer, Kresge, Kent
+        final Marker bpac = map.addMarker(new MarkerOptions().position(bpacLatLng)
+                .title("BPAC"));
+        final Marker kent = map.addMarker(new MarkerOptions().position(kentLatLng)
+                .title("Kent Campus Center"));
+        final Marker lekburgHall = map.addMarker(new MarkerOptions().position(lekburgHallLatLng)
+                .title("Lekburg Recital Hall"));
+        final Marker greatPfieffer = map.addMarker(new MarkerOptions().position(greatPfiefferLatLng)
+                .title("Great Hall/Pfieffer"));
+        final Marker buxtonStadium = map.addMarker(new MarkerOptions().position(buxtonStadiumLatLng)
+                .title("Bill Buxton Stadium"));
+        final Marker dunnLibrary = map.addMarker(new MarkerOptions().position(dunnLibraryLatLng)
+                .title("Dunn Library"));
 
         wallaceHall.setTag(wallaceHallLatLng);
         maryBerry.setTag(maryBerryLatLng);
         carverHall.setTag(carverHallLatLng);
         collegeHall.setTag(collegeHallLatLng);
-        amyRobinson.setTag(amyRobinsonLatLng);
         smithChapel.setTag(smithChapelLatLng);
         hopperGym.setTag(hopperGymLatLng);
         cowlesFieldhouse.setTag(cowlesLatLng);
         mcNeillHall.setTag(mcNeillHallLatLng);
         hillmanHall.setTag(hillmanHallLatLng);
-        // TO DO - (setTag) Barker, Great/Pfieffer, Kresge, Kent
-
-
+        bpac.setTag(bpacLatLng);
+        kent.setTag(kentLatLng);
+        lekburgHall.setTag(lekburgHallLatLng);
+        greatPfieffer.setTag(greatPfiefferLatLng);
+        buxtonStadium.setTag(buxtonStadiumLatLng);
+        dunnLibrary.setTag(dunnLibraryLatLng);
         // Wallace Marker clicker
         map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
                 if(marker.getTag() == wallaceHallLatLng)
                 {
-                    // change to wallacehistory
-                    Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
-                    startActivity(intent);
-
+                    setContentView(R.layout.activity_wallace);
                 }
-                return false;
-            }
-        });
-
-        // Mary Berry Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == maryBerryLatLng)
+                else if(marker.getTag() == maryBerryLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // Carver hall Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == carverHallLatLng)
+                else if(marker.getTag() == carverHallLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // College Hall Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == collegeHallLatLng)
+                else  if(marker.getTag() == collegeHallLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // Smith Chapel CLicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == smithChapelLatLng)
+                else  if(marker.getTag() == smithChapelLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // Hopper Gym Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == hopperGymLatLng)
+                else  if(marker.getTag() == hopperGymLatLng)
+                {
+                    setContentView(R.layout.activity_hopper);
+                }
+                else if(marker.getTag() == cowlesLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
+                else  if(marker.getTag() == mcNeillHallLatLng)
+                {
+                    setContentView(R.layout.activity_mcneil);
 
-        // Cowles Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == cowlesLatLng)
+
+                }
+                else  if(marker.getTag() == hillmanHallLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // McNeill Hall Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == mcNeillHallLatLng)
+                else  if(marker.getTag() == bpacLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // Hillman Hall Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == hillmanHallLatLng)
+                else  if(marker.getTag() == kentLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // BPAC Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == bpacLatLng)
+                else  if(marker.getTag() == lekburgHallLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
                     startActivity(intent);
                 }
-                return false;
-            }
-        });
-
-        // Kent Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == kentLatLng)
+                else  if(marker.getTag() == greatPfiefferLatLng)
                 {
-                    // change to wallacehistory
-                    Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
-                    startActivity(intent);
+                    setContentView(R.layout.activity_greathall);
                 }
-                return false;
-            }
-        });
-
-        // Lekburg Hall Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == lekburgHallLatLng)
+                else  if(marker.getTag() == buxtonStadiumLatLng)
                 {
-                    // change to wallacehistory
-                    Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
-                    startActivity(intent);
+                    setContentView(R.layout.activity_bill_buxton);
                 }
-                return false;
-            }
-        });
-
-        // Great Hall/ Pfieffer Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == greatPfiefferLatLng)
-                {
-                    // change to wallacehistory
-                    Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
-                    startActivity(intent);
-                }
-                return false;
-            }
-        });
-
-        // Buxton Stadium Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == buxtonStadiumLatLng)
-                {
-                    // change to wallacehistory
-                    Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
-                    startActivity(intent);
-                }
-                return false;
-            }
-        });
-
-        // Dunn Library Clicker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(marker.getTag() == dunnLibraryLatLng)
+                else if(marker.getTag() == dunnLibraryLatLng)
                 {
                     // change to wallacehistory
                     Intent intent = new Intent(MapActivity.this, MapPopActivity.class);
@@ -327,7 +268,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         });
 
     }
-
 
     public void onLocationChanged(Location location) {
         // You can now create a LatLng Object for use with maps
@@ -347,11 +287,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     }
 
-    public void click_Home(View view) {
-        setContentView(R.layout.activity_main_home);
-
+    private BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable) {
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
-
     private boolean checkPermissions() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -374,6 +317,11 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+//    public void click_BackBuilding(View view)
+//    {
+//        setContentView(R.layout.activity_map);
+//    }
 
 
 
